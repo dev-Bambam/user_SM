9a6a1086e95176d27e7fd69e844cc1d8f3690fc5
dev-Bambam <ayogood18@gmail.com>
2024-10-10 11:23:50 +0100

    unlearnig to learn

--- Database.php
+++ Database.php
@@ -5,11 +5,3 @@ $password = "";
 $db = "bambam";
 
 
-$mysqli = new mysqli($host, $user, $password, $db);
-
-if ($mysqli->connect_error) {
-    die("conection failed: $mysqli->connect_error");
-}
-
-
-// $mysqli->close();
